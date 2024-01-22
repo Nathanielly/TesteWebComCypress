6897d68bacd6516e333d971cc513d05c1b14957f
Nathanielly <nathanielly23@hotmail.com>
2024-01-22 20:58:13 -0300

    Entrega de projeto

---
+++ cypress/e2e/PesquisarProduto.cy.js
@@ -0,0 +1,17 @@
+describe('PesquisarProduto', () => {
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('Pesquisar um produto', () => {
+        
+        cy.get('#txtDsKeyWord').type('Orquídea')
+        cy.get('span#btnSearch').click()
+        cy.get('div.close-button').click()
+        cy.get('[href="https://www.giulianaflores.com.br/orquidea-azul/p27330/?src=DEPT"] > .title-item').click()
+        cy.get('#ContentSite_lblProductDsName').should('have.text', 'Orquídea Azul');
+        cy.get('.preco_prod > .precoPor_prod').should('have.text', '\n                        R$ 266,31');
+        
+    })
+})
